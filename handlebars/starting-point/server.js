cb1c8f42b7968c721dfa9cc47316eebd64ee9056
Shahzad <shasa3f@ad.vzwcorp.com>
2021-12-13 20:47:54 -0500

    first commit

--- handlebars/starting-point/server.js
+++ handlebars/starting-point/server.js
@@ -18,7 +18,15 @@ const port = 3000;
 
 app.use(express.static('public'));
 
-app.use(express.json());
+// app.use(express.json());
+//Configures handlebars library to work well w/ Express + Sequelize model
+const handlebars = expressHandlebars({
+    handlebars : allowInsecurePrototypeAccess(Handlebars)
+})
+
+//Tell this express app we're using handlebars
+app.engine('handlebars', handlebars);
+app.set('view engine', 'handlebars')
 
 
 const restaurantChecks = [
@@ -28,17 +36,17 @@ const restaurantChecks = [
 ]
 
 app.get('/restaurants', async (req, res) => {
-    const restaurants = await Restaurant.findAll();
-    res.json(restaurants);
+    const restaurantList = await Restaurant.findAll()
+    res.render('restaurants',{restaurantList} )
 });
 
 app.get('/restaurants/:id', async (req, res) => {
-    const restaurant = await Restaurant.findByPk(req.params.id, {include: {
+    const thisrestaurant = await Restaurant.findByPk(req.params.id, {include: {
             model: Menu,
             include: MenuItem
         }
     });
-    res.json(restaurant);
+    res.render("oneRestaurant", {thisrestaurant});
 });
 
 app.post('/restaurants', restaurantChecks, async (req, res) => {
